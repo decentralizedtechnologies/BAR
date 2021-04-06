0238ce8add2e4e3c3a3f1c0557cd0f5ebc3ba2dc
Gustavo Ibarra <gus@developermexicano.com>
2021-04-06 17:17:41 -0600

    chore: fixes import

--- src/ui/modal/closeButton/CloseButton.types.ts
+++ src/ui/modal/closeButton/CloseButton.types.ts
@@ -1,11 +1,10 @@
-import { CSSProperties } from 'react';
-
-import { ButtonProps } from 'ui/form/button/Button.types';
+import { CSSProperties } from "react";
+import { ButtonProps } from "../../button/Button.types";
 
 export type CloseButtonProps = {
   className?: string;
   style?: CSSProperties;
-  variant?: ButtonProps['variant'];
-  color?: ButtonProps['color'];
+  variant?: ButtonProps["variant"];
+  color?: ButtonProps["color"];
   onClick(): void;
 };
